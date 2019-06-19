6dad88d078f866478299eb53491ca25d42cf1e63
Thiago Santiago <thiago.vps20@gmail.com>
2019-06-19 09:15:01 -0300

    bugFix

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9d5b0284f3d2a20013272311de8c78f9.js"
+  "precache-manifest.bf55ddf137e82a15fb244a5ce8253639.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
